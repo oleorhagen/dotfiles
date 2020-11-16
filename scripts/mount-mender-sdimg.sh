37aebbc6a0017293fab89689f227a1d32bea895a
Ole Petter <ole.orhagen@northern.tech>
2020-11-16 15:25:26 +0100

    Prettied up
    Changelog: None
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- scripts/mount-mender-sdimg.sh
+++ scripts/mount-mender-sdimg.sh
@@ -24,54 +24,59 @@ while [[ $# -ne 0 ]]; do
 done
 
 echo >&2 "Reading the addresses from $IMAGE"
-start_addresses=$(fdisk -l "$IMAGE" | awk 'BEGIN {DEVICE=false}
-$1 == "Device" {DEVICE=true }
-{if (DEVICE == true)
-    if ($2 == "*")
-       printf $3
-    else
-       print $2
-}')
+start_addresses=($(fdisk -l "$IMAGE" | awk '
+{if (DEVICE)
+    if ($2 == "*") {
+           print $3
+       } else {
+           print $2
+       }
+}
+$1 == "Device" {DEVICE="True"}
+'))
 
-echo "start addresses: $start_addresses"
-exit 1
+echo -e "start addresses:\n${start_addresses[0]}"
+echo -e "start addresses:\n${start_addresses[1]}"
+echo -e "start addresses:\n${start_addresses[2]}"
+echo -e "start addresses:\n${start_addresses[3]}"
 
 cat <<EOF
 Which partition do you whish to mount:
 
-1 - /boot
-2 - /rootfsa
-3 - /rootfsb
-4 - /data
+0 - /boot
+1 - /rootfsa
+2 - /rootfsb
+3 - /data
 EOF
+
+
 read PARTITION_NR
-for start_address in ${start_addresses}; do
-	case ${PARTITION_NR} in
-		0)
-      echo >&2 "Mounting the boot partition to /mnt/boot"
-			MNT=/mnt/boot/
-			;;
-		1)
-      echo >&2 "Mounting the /rootfsa partition to /mnt/rootfsa"
-			MNT=/mnt/rootfsa/
-			;;
-		2)
-      echo >&2 "Mounting the /rootfsb partition to /mnt/rootfsb"
-			MNT=/mnt/rootfsb/
-			;;
-		3)
-      echo >&2 "Mounting the /data partition to /mnt/data"
-			MNT=/mnt/data/
-			;;
-		*)
-			echo >&2 "$i not a valid index"
-			exit 2
-	esac
-	if [[ -n "${UMOUNT}" ]]; then
-    echo >&2 "Unmounting ${MNT}"
-		sudo umount "${MNT}"
-	else
-		echo >&2 "mounting: mount -o loop,offset=$((512*start_address)) ${IMAGE} ${MNT}"
-		sudo mount -o loop,offset=$((512*start_address))  "${IMAGE}" "${MNT}"
-	fi
-done
+
+case ${PARTITION_NR} in
+  0)
+    echo >&2 "Mounting the boot partition to /mnt/boot"
+    MNT=/mnt/boot/
+    ;;
+  1)
+    echo >&2 "Mounting the /rootfsa partition to /mnt/rootfsa"
+    MNT=/mnt/rootfsa/
+    ;;
+  2)
+    echo >&2 "Mounting the /rootfsb partition to /mnt/rootfsb"
+    MNT=/mnt/rootfsb/
+    ;;
+  3)
+    echo >&2 "Mounting the /data partition to /mnt/data"
+    MNT=/mnt/data/
+    ;;
+  *)
+    echo >&2 "$i not a valid index"
+    exit 2
+esac
+if [[ -n "${UMOUNT}" ]]; then
+  echo >&2 "Unmounting ${MNT}"
+  sudo umount "${MNT}"
+else
+  echo >&2 "mounting: mount -o loop,offset=$((512*${start_addresses[${PARTITION_NR}]})) ${IMAGE} ${MNT}"
+  sudo mount -o loop,offset=$((512*${start_addresses[${PARTITION_NR}]}))  "${IMAGE}" "${MNT}"
+fi
