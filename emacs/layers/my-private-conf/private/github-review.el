fe7ee86a05a02dae9585b0a1f60d316d96e74f7f
Ole Petter <ole.orhagen@northern.tech>
2021-02-09 09:59:04 +0100

    Add PoC github review code
    Changelog: None

--- emacs/layers/my-private-conf/private/github-review.el
+++ emacs/layers/my-private-conf/private/github-review.el
@@ -6,4 +6,49 @@
 
 (defun github-review-checkout-pr (number)
   (interactive "n")
-  (shell-command (format-string "gh pr checkout %s" number)))
+  (shell-command (format "gh pr checkout %s" number)))
+
+
+(defun extract-files-in-commit (commit)
+  (shell-command-to-string (format "git diff-tree --no-commit-id --name-only -r %s" commit)))
+
+;; Open the files in the PR in a different buffer
+
+;; (require 'persp) - Not needed I think !
+(require 'magit)
+
+;; Insert the keybinding 'v' to view a commit as a reviewer (!)
+
+;; (transient-append-suffix 'magit-log "a"
+;;   '("w" "Wip" magit-wip-log-current))
+
+(defvar persp-review-name "review" "This is the name of the review perspective")
+(transient-append-suffix 'magit-dispatch "_"
+  '("v" "reView" review-commit))
+
+(defun current-commit ()
+  (shell-commit-to-string (format "git rev-parse HEAD")))
+
+(defun open-files-in-new-perspective (files)
+  (interactive "b")
+  (persp-switch "baz")
+  (defun ))
+
+(defun do-review-commit (commit)
+  (let ((files (extract-files-in-commit commit)))
+    (open-files-in-new-perspective files)))
+
+(defun do-review (number)
+  (interactive "n")
+  (github-review-checkout-pr number))
+
+(defun review-commit ()
+  (interactive)
+  (do-review)
+  (let ((commit (current-commit))
+        (files (extract-files-in-commit commit)))
+    (open-files-in-new-perspective files)))
+
+(defun review-close ()
+  (interactive)
+  (persp-kill 'persp-review-name))
