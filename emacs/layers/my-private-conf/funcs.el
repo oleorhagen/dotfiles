ecadb354e243e871165366bd11e2980bac63c904
Ole Petter <ole.orhagen@northern.tech>
2021-02-09 09:59:46 +0100

    No need to save-excursion twice
    Changelog: None

--- emacs/layers/my-private-conf/funcs.el
+++ emacs/layers/my-private-conf/funcs.el
@@ -5,11 +5,9 @@
 ;;
 
 (defun update-license-year ()
-  (interactive)
-  (save-excursion
-    (beginning-of-buffer)
-    (when (re-search-forward "^\\(#\\|;;\\|//\\)\\( +Copyright +\\)\\([0-9]\\{4\\}\\)" nil nil)
-      (replace-match (format-time-string "\\1\\2%Y")))))
+  (beginning-of-buffer)
+  (when (re-search-forward "^\\(#\\|;;\\|//\\)\\( +Copyright +\\)\\([0-9]\\{4\\}\\)" nil nil)
+    (replace-match (format-time-string "\\1\\2%Y"))))
 
 ;; Add the hook to after-save
 
