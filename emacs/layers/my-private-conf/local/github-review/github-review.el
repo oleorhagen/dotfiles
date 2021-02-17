7ded80ee06e4f3f24f8e265c923119748c1bd4e1
Ole Petter <ole.orhagen@northern.tech>
2021-02-17 10:46:19 +0100

    Needs to be in local
    Changelog: None
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- emacs/layers/my-private-conf/local/github-review/github-review.el
+++ emacs/layers/my-private-conf/local/github-review/github-review.el
@@ -3,6 +3,10 @@
 ;; Simple functionality to help review pull-requests in Emacs
 ;;
 
+;; TODO - Prettier interface to start a review
+;; TODO - Don't start all the interactive LSP stuff when reviewing - highlighting only
+;; TODO - Highlight the changes in some colour/shade
+
 
 (defun github-review-checkout-pr (number)
   (interactive "n")
@@ -22,21 +26,23 @@
 ;; (transient-append-suffix 'magit-log "a"
 ;;   '("w" "Wip" magit-wip-log-current))
 
-(defvar persp-review-name "review" "This is the name of the review perspective")
+(defvar persp-review-name "github-review" "This is the name of the review perspective")
+
 (transient-append-suffix 'magit-dispatch "_"
   '("v" "reView" review-commit))
 
 (defun current-commit ()
-  (shell-commit-to-string (format "git rev-parse HEAD")))
+  (magit-copy-section-value nil))
 
 (defun open-files-in-new-perspective (files)
-  (interactive "b")
-  (persp-switch "baz")
-  (defun ))
-
-(defun do-review-commit (commit)
-  (let ((files (extract-files-in-commit commit)))
-    (open-files-in-new-perspective files)))
+  (persp-switch "github-review")
+  (defun iter-open (files)
+    (unless (null files)
+      (message "Opening %s for review" (expand-file-name (car files)))
+      (find-file-noselect (car files))
+      (iter-open (cdr files))))
+  (iter-open files)
+  (switch-to-buffer (car files)))
 
 (defun do-review (number)
   (interactive "n")
@@ -44,11 +50,13 @@
 
 (defun review-commit ()
   (interactive)
-  (do-review)
-  (let ((commit (current-commit))
-        (files (extract-files-in-commit commit)))
+  (let* ((commit (current-commit))
+        (files (split-string (extract-files-in-commit commit))))
     (open-files-in-new-perspective files)))
 
-(defun review-close ()
+(defun github-review-close ()
   (interactive)
-  (persp-kill 'persp-review-name))
+  (persp-kill "github-review"))
+
+
+(provide 'github-review)
