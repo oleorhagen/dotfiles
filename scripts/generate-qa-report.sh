3c20102cee07dfb3507cb42ae581111baf9f266f
Ole Petter <ole.orhagen@northern.tech>
2022-01-03 15:07:54 +0100

    Improve the monthly report script
    Changelog: None
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- scripts/generate-qa-report.sh
+++ scripts/generate-qa-report.sh
@@ -62,60 +62,7 @@ echo >&2 "${NEW_ISSUES}"
 ####################
 
 # Tac this, and create a simple graphic for the red and green nightlies
-
-# Get all the scheduled pipelines from the last month
-function gitlab-get-nightlies() {
-    local -r PREVIOUS_MONTH=$(date --date "$(date +%Y-%m-01) -1 day" +%Y-%m)
-    local -r THIS_MONTH=$(date --date "$(date +%Y-%m-01)" +%Y-%m)
-    curl --header "PRIVATE-TOKEN: $(pass show private/gitlab/access-token)" "https://gitlab.com/api/v4/projects/12501706/pipelines.json?per_page=100&updated_after=${PREVIOUS_MONTH}-01&updated_before=${THIS_MONTH}-01&source=schedule"
-
-}
-
-
-function gitlab-nightly-stats() {
-
-    # Get the stats
-    NIGHTLIES=$(gitlab-get-nightlies | jq -j '.[] | .created_at," ",.status,"\n"' | tac)
-
-    #
-    # Per day overview
-    #
-    echo -e "Day\tStatus"
-    echo "---------------"
-    OIFS=${IFS}
-    IFS=$'\n'
-    for day in ${NIGHTLIES}; do
-        # Show the nightly day, and the status
-        echo -e -n $(echo -n "${day}" | cut -d' ' -f1 | awk -F'-' '{print $3}' | awk -FT '{print $1}') "\t"
-        echo " ${day}" | cut -d' ' -f3
-        # TODO - add emojis
-        # RES=$(echo " ${day}" | cut -d' ' -f3)
-        # if [[ "${RES}" = "failed" ]]; then
-        #     printf '\U1F534'
-        # fi
-    done
-    IFS=${OIFS}
-
-    local -r NR_OF_DAYS_IN_PREVIOUS_MONTH=$(date --date "$(date +%Y-%m-01) -1 day" +%d)
-
-    #
-    # Stats
-    #
-    echo
-    echo "### Simple statistics"
-    echo
-    echo "${NIGHTLIES}" | awk -v DAYS=${NR_OF_DAYS_IN_PREVIOUS_MONTH} \
-'
-$2 == "failed" {failed += 1}
-$2 == "success" {passed += 1}
-END {
-print "passed: " passed
-print "failed: " failed
-print "Success ratio: " passed/DAYS
-print "Failure ratio: " failed/DAYS
-
-}'
-}
+source /home/olepor/dotfiles/scripts/qa-report-gitlab-util.sh
 
 cat <<EOF
 # Mender QA - <Month>
@@ -135,5 +82,9 @@ ${NEW_ISSUES}
 ## GitLab Nightlies:
 
 $(gitlab-nightly-stats)
+
+## GitLab Nightlies failing tests:
+
+$(gitlab-pipeline-trouble-statistics)
 
 EOF
---
+++ scripts/qa-report-gitlab-util.sh
@@ -0,0 +1,91 @@
+
+# Get all the scheduled pipelines from the last month
+function gitlab-get-nightlies() {
+    local -r PREVIOUS_MONTH=$(date --date "$(date +%Y-%m-01) -1 day" +%Y-%m)
+    local -r THIS_MONTH=$(date --date "$(date +%Y-%m-01)" +%Y-%m)
+    curl --header "PRIVATE-TOKEN: $(pass show private/gitlab/access-token)" "https://gitlab.com/api/v4/projects/12501706/pipelines.json?per_page=100&updated_after=${PREVIOUS_MONTH}-01&updated_before=${THIS_MONTH}-01&source=schedule"
+}
+
+function gitlab-nightlies-get() {
+    local -r NIGHTLIES="$1"
+    local -r key="$2"
+    echo $NIGHTLIES | jq ".[] | ${key}"
+}
+
+function get-pipeline-errors-or-failures() {
+    local -r pipeline_ids=$1
+    local failing_tests=()
+    for pipeline_id in $(echo $1); do
+        local GITLAB_PIPELINE_ID="$pipeline_id"
+        echo >&2 "GITLAB_PIPELINE_ID: ${GITLAB_PIPELINE_ID}"
+        local new_tests_for_id="$(curl --fail --header "PRIVATE-TOKEN: $(pass show private/gitlab/access-token)" "https://gitlab.com/api/v4/projects/12501706/pipelines/${GITLAB_PIPELINE_ID}/test_report" | jq '.test_suites | .[] | .test_cases | .[] | select(.status=="error" or .status=="failed") | .name')"
+        failing_tests=("${failing_tests}" "${new_tests_for_id}")
+        # Don't hammer the API too hard - TODO - consider graphQL
+        sleep 1
+    done
+    # extract the failing test stats
+    echo >&2 ${failing_tests}
+    # AWK the shit out of these statistics!
+    echo ${failing_tests} | awk '
+{
+        failures[$1]++
+}
+END {
+    for (name in failures) {
+            printf("%s=%d\n",name, failures[name])
+    }
+}
+' | sort -t= --key=2 --numeric-sort --reverse
+}
+
+function gitlab-pipeline-trouble-statistics() {
+    NIGHTLIES="$(gitlab-get-nightlies)"
+    PIPELINE_IDS=$(gitlab-nightlies-get "${NIGHTLIES}" .id)
+    get-pipeline-errors-or-failures ${PIPELINE_IDS}
+}
+
+
+function gitlab-nightly-stats() {
+
+    # Get the stats
+    NIGHTLIES=$(gitlab-get-nightlies | jq -j '.[] | .created_at," ",.status,"\n"' | tac)
+
+    #
+    # Per day overview
+    #
+    echo -e "Day\tStatus"
+    echo "---------------"
+    OIFS=${IFS}
+    IFS=$'\n'
+    for day in ${NIGHTLIES}; do
+        # Show the nightly day, and the status
+        echo -e -n $(echo -n "${day}" | cut -d' ' -f1 | awk -F'-' '{print $3}' | awk -FT '{print $1}') "\t"
+        echo " ${day}" | cut -d' ' -f3
+        # TODO - add emojis
+        # RES=$(echo " ${day}" | cut -d' ' -f3)
+        # if [[ "${RES}" = "failed" ]]; then
+        #     printf '\U1F534'
+        # fi
+    done
+    IFS=${OIFS}
+
+    local -r NR_OF_DAYS_IN_PREVIOUS_MONTH=$(date --date "$(date +%Y-%m-01) -1 day" +%d)
+
+    #
+    # Stats
+    #
+    echo
+    echo "### Simple statistics"
+    echo
+    echo "${NIGHTLIES}" | awk -v DAYS=${NR_OF_DAYS_IN_PREVIOUS_MONTH} \
+'
+$2 == "failed" {failed += 1}
+$2 == "success" {passed += 1}
+END {
+print "passed: " passed
+print "failed: " failed
+print "Success ratio: " passed/DAYS
+print "Failure ratio: " failed/DAYS
+
+}'
+}
