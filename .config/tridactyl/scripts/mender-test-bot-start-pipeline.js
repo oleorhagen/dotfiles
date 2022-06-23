9e1fd78cd397a85a1e6a5cc17cb1a2159a8959b4
Ole Petter <ole.orhagen@northern.tech>
2022-06-23 17:08:26 +0200

    update the start pipeline script
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- .config/tridactyl/scripts/mender-test-bot-start-pipeline.js
+++ .config/tridactyl/scripts/mender-test-bot-start-pipeline.js
@@ -1,20 +1,10 @@
-let comment_field = document.getElementById('new_comment_field');
+let comment_field = document.getElementById("new_comment_field");
 
-comment_field.value = '@mender-test-bot start pipeline';
+comment_field.value = "@mender-test-bot start pipeline";
 
 console.log(comment_field);
 
-// Steal the focus
-comment_field.dispatchEvent(new Event('focus', {
-    bubbles: true
-}));
+let comment_button = document.getElementsByClassName("btn-primary btn")[2];
 
-// Pretend we've manually entered the text
-comment_field.dispatchEvent(new Event('input', {
-    bubbles: true
-}));
-
-// Unfocus to enable the comment button
-comment_field.dispatchEvent(new Event('blur', {
-    bubbles: true
-}));
+// Set the button to enabled
+comment_button.disabled = false;
