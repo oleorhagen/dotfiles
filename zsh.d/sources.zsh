3bab270ced20e71193c3318548399b3d539a9bec
Ole Petter <ole.orhagen@northern.tech>
2022-06-23 17:09:48 +0200

    add gcloud zsh.d sources script
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

---
+++ zsh.d/sources.zsh
@@ -0,0 +1,5 @@
+
+# Gcloud setup path and completion
+source /home/olepor/aur/google-cloud-sdk/pkg/google-cloud-sdk/opt/google-cloud-sdk/completion.zsh.inc
+source /home/olepor/aur/google-cloud-sdk/pkg/google-cloud-sdk/opt/google-cloud-sdk/path.zsh.inc
+
