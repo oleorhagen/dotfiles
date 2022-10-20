d24d31221237adbfc069ea9595c32838693b491b
Ole Petter <ole.orhagen@northern.tech>
2022-10-20 14:55:20 +0200

    magit: Add a function to cherry pick to branches
    As opposed to just fetching
    
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- emacs/work-utilites/magit-cherry.el
+++ emacs/work-utilites/magit-cherry.el
@@ -2,7 +2,7 @@
 
 
 (transient-append-suffix 'magit-dispatch "A"
-  '("t" "backports" my-magit-cherry-multiple))
+  '("t" "backports" my-magit-cherry-to))
 
 (defun my-magit-cherry-multiple (name branches commits  &optional args)
   "Move COMMITS from the current branch onto another existing BRANCH.
@@ -31,3 +31,27 @@ to fix that and finish the process manually."
                             "--base" (string-remove-prefix "mendersoftware/" branch)
                             "--body" "Cherry"
                             "--draft")))))
+
+
+
+;; (defun )
+
+(defun my-magit-cherry-to (target-branch commits  &optional args)
+  "Create a branch on the target BRANCH, and then cherry-pick the
+given commits to it. The BRANCH will be named as
+<cherry-[target-cherry-pick-branch]>. If a conflict occurs you
+will have to resolve this manually yourself."
+  ;; Extract the arguments interactively
+  (interactive
+   (list
+    ;; Helm (magit) get branch
+    (magit-completing-read-multiple* "Cherry-pick to branch: " (magit-list-branch-names))
+    ;; Get the commits
+    (magit-cherry-pick-read-args "Cherry-pick: ")))
+  (progn
+    (magit-run-git
+     "checkout" "-B" (format "cherry-%s" (car target-branch)) target-branch "--no-track")
+    (magit-run-git "cherry-pick" commits)
+    (magit-push-current (format "cherry-%s" (car target-branch)) target-branch)
+    ;; TODO - Use forge to automatically create a PR
+    (forge-create-pullreq source-branch target-branch)))
