be35098b02bb2a49b2bb344f3fb22587c4164b11
Ole Petter <ole.orhagen@northern.tech>
2024-01-14 13:30:34 +0100

    emacs(my-layer): Stop using the automatic license year change
    We have since moved away from this.
    
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- emacs/layers/my-private-conf/funcs.el
+++ emacs/layers/my-private-conf/funcs.el
@@ -1,24 +1,24 @@
 
 ;;
 ;;; This is a simple snippet to replace the license year header with the current
-;;; year.
+;;; year (EOL) - we no longer update the year of the License Header
 ;;
 
-(defun update-license-year ()
-  (save-excursion
-    (when (memq this-command '(save-buffer save-some-buffers))
-      (beginning-of-buffer)
-      (when (re-search-forward "^\\(#\\|;;\\|//\\)\\( +Copyright +\\)\\([0-9]\\{4\\}\\)" nil nil)
-        (replace-match (format-time-string "\\1\\2%Y"))))))
+;; (defun update-license-year ()
+;;   (save-excursion
+;;     (when (memq this-command '(save-buffer save-some-buffers))
+;;       (beginning-of-buffer)
+;;       (when (re-search-forward "^\\(#\\|;;\\|//\\)\\( +Copyright +\\)\\([0-9]\\{4\\}\\)" nil nil)
+;;         (replace-match (format-time-string "\\1\\2%Y"))))))
 
-;; Add the hook to before-save
+;; ;; Add the hook to before-save
 
-(add-hook 'before-save-hook
-          (lambda ()
-            (when (memq this-command '(save-buffer save-some-buffers))
-              (save-excursion
-                (beginning-of-buffer)
-                (when (looking-at "^\\(#\\|;;\\|//\\)\\( +Copyright +\\)\\([0-9]\\{4\\}\\) +Northern.tech")
-                  (update-license-year))))))
+;; (add-hook 'before-save-hook
+;;           (lambda ()
+;;             (when (memq this-command '(save-buffer save-some-buffers))
+;;               (save-excursion
+;;                 (beginning-of-buffer)
+;;                 (when (looking-at "^\\(#\\|;;\\|//\\)\\( +Copyright +\\)\\([0-9]\\{4\\}\\) +Northern.tech")
+;;                   (update-license-year))))))
 
 
