cddb4ea3551ec7d27a4ff6ca453622f632e6b92e
Ole P. Orhagen <ole.petter.orhagen@mimiro.no>
2024-04-04 13:33:11 +0200

    emacs(private-conf): Allow creating missing test-files when <spc>-p-a
    Signed-off-by: Ole P. Orhagen <ole.petter.orhagen@mimiro.no>

--- emacs/layers/my-private-conf/layers.el
+++ emacs/layers/my-private-conf/layers.el
@@ -8,3 +8,9 @@
   (configuration-layer/declare-layer 'git)
 
   )
+
+;; Allow creating missing test-files using projectil
+(when (configuration-layer/package-used-p 'spacemacs-project)
+  (customize-set-variable 'projectile-create-missing-test-files t)
+)
+
