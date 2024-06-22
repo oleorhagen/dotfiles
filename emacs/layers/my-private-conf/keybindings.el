bdc46b6b096e040f65e886c4130874ec83055db4
Ole P. Orhagen <ole.petter.orhagen@mimiro.no>
2024-06-22 22:47:13 +0200

    emacs(private-layer): Make return follow links in info-mode
    Signed-off-by: Ole P. Orhagen <ole.petter.orhagen@mimiro.no>

--- emacs/layers/my-private-conf/keybindings.el
+++ emacs/layers/my-private-conf/keybindings.el
@@ -1,5 +1,9 @@
-
-
 ;; Add a key for listing the current available snippets
 (spacemacs/set-leader-keys
-    "os" 'yas-describe-tables)
+  "os" 'yas-describe-tables)
+
+;;; Make return follow link in info-mode
+(add-hook 'Info-mode-hook
+          (lambda ()
+            (define-key evil-motion-state-local-map
+                        (kbd "<return>") 'Info-follow-nearest-node)))
