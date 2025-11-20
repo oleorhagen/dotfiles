4322edbc90ff1f208c0b3fb4ff4b13c667bb40ed
Ole P. Orhagen <ole@orhagen.no>
2025-11-20 20:31:06 +0100

    chore(nix): Setup the mdns conf in network.nix

--- nix/network.nix
+++ nix/network.nix
@@ -2,7 +2,22 @@
 
 {
 
-  networking.interfaces.eth.useDHCP = false;
-  networking.extraHosts = "127.0.0.1 skjeberg-invitational.com";
+  # networking.interfaces.eth.useDHCP = false;
+  # networking.extraHosts = "127.0.0.1 skjeberg-invitational.com";
+
+  # Enable mDNS for .local address resolution
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true; # Enable mDNS for IPv4
+    nssmdns6 = true; # Enable mDNS for IPv6
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
+  };
 
 }
